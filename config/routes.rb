6aab2b885486ef9d707b6ee9c73782c141d1f5db
miriam <miriam5000@gmail.com>
2018-04-13 11:37:36 -0400

    extended devise_for in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => 'callbacks'}
   resources :products
   root 'product#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
